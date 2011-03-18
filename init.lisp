8971a26cb56400de04fadc3b314f3e14991d2cf1
Varoun P <commits@varoun.com>
2011-03-18 20:13:26 +0530

    Modified init.lisp to load schema.lisp.

--- init.lisp
+++ init.lisp
@@ -5,6 +5,7 @@
 (defvar *files* 
   '(#p"/home/varoun/Code/groklogs-ng/packages.lisp"
     #p"/home/varoun/Code/groklogs-ng/utils.lisp"
+    #p"/home/varoun/Code/groklogs-ng/schema.lisp"
     #p"/home/varoun/Code/groklogs-ng/preprocess.lisp"
     #p"/home/varoun/Code/groklogs-ng/learn.lisp"))
 
